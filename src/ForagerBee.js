aa8902e372ac98aab22d9a2ceb7d0748397b9f75
Tom Spitz <tjspitz1986@gmail.com>
2022-12-19 12:52:47 -0600

    We have all the Bees, aka BeesBeesBees.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+  // call the parent class (still Bee)
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// inherit methods
+ForagerBee.prototype = Object.create(Bee.prototype)
+
+// point constructor to self
+ForagerBee.prototype.constructor = ForagerBee
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
