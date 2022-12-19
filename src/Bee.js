ad964291b6de04408a69f1e65f3ab59f09a929e0
Tom Spitz <tjspitz1986@gmail.com>
2022-12-19 12:36:52 -0600

    We have Grubs, Bees, and Honeymaker Bees.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+// inherit methods from Grub
+Bee.prototype = Object.create(Grub.prototype)
+
+// point the constructor to the Bee (not Grub)
+Bee.prototype.constructor = Bee
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  // call the parent class to construct
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype)
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot ++;
+  return this.honeyPot;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot --;
+  return this.honeyPot;
+}
